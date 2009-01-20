18494fdafc719ea4c330bf683ff7d699881f8d33
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2009-01-20 12:30:45 -0200

    - Sample nawk processing scripts for gateway SIF files (records 2D & 4A).

---
+++ 2d_record.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/ksh
+## This shell script extracts 2D charges from a given
+## Input File which is in the Gateway SIF format.
+
+file_nm=$1
+
+nawk '{
+	record_ty=substr($0,121,2);
+	svc_reqst_desc=substr($0,80,20);
+	
+	if (( record_ty == "2D" ) && ( svc_reqst_desc=="CONTR LEVEL DISCOUNT"))
+	{
+		print $0;
+	}
+
+}' $file_nm 
+
---
+++ 4a_record.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/ksh
+## This shell script extracts 2D charges from a given
+## Input File which is in the Gateway SIF format.
+
+file_nm=$1
+
+nawk '{
+	record_ty=substr($0,121,2);
+	entity=substr($0,132,2);
+	if (record_ty == "2D") {
+		svc_nm=substr($0,136,45);
+	}
+	if ((record_ty == "4A") && (entity == "PO")) {
+		circuit_id=substr($0,285,30);
+		svc_nm_desc=substr($0,134,45);
+		print "[" svc_nm_desc "] [" circuit_id "]";
+	}
+}' $file_nm 
+
